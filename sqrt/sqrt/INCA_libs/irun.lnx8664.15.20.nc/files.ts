4410f3d7f529499b696a8c362b58485aaee6daed
sunidhi07 <59998582+sunidhi07@users.noreply.github.com>
2022-04-23 18:37:08 +0530

    Delete sqrt directory

--- sqrt/sqrt/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,8 +0,0 @@
-1649403053 /home/adld27/sqrt/cds.lib
-1649402717 /home/adld27/sqrt/hdl.var
-1631699397 /home/adld27/cds.lib
-1649324166 /home/adld27/sqrt/sqrttb.v
-1649324837 /home/adld27/sqrt/sq2.v
-1649402542 /home/adld27/sqrt/sqrt1.v
-1649402454 /home/adld27/sqrt/try1.v
-1649655512 /home/adld27/sqrt/tb.v
